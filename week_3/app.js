8920449000d873c2269e37c47d8bf71934d46a70
AvansIvar <igm.schreutelkamp@student.avans.nl>
2025-02-17 15:15:28 +0100

    Enhance: Basic routing work in progress.

--- week_3/app.js
+++ week_3/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
 var server = app.listen(5000, function(){
